7ed7de3db6f697f227995095898e1d2c878cc984
nao-0802 <tky2302123@stu.o-hara.ac.jp>
2024-11-12 11:42:15 +0900

    bean完成だよん

--- src/bean/ExcretionRecord.java
+++ src/bean/ExcretionRecord.java
@@ -1,5 +1,47 @@
 package bean;
 
-public class ExcretionRecord {
+import java.sql.Date;
 
+public class ExcretionRecord implements java.io.Serializable{
+
+	private String excretion_id;
+	private String student_id;
+	private Date day;
+	private Date time;
+	private int type;
+	private String excretion_detail;
+
+	public String getExcretionId(){
+		return excretion_id;
+	}
+	public String getStudentId(){
+		return student_id;
+	}
+	public Date getDay(){
+		return day;
+	}
+	public Date getTime(){
+		return time;
+	}
+	public int getType(){
+		return type;
+	}
+	public String getExcretionDetail(){
+		return excretion_detail;
+	}
+	public void setExcretionId(String excretion_id){
+		this.excretion_id=excretion_id;
+	}
+	public void setDay(Date day){
+		this.day=day;
+	}
+	public void setTime(Date time){
+		this.time=time;
+	}
+	public void setType(int type){
+		this.type=type;
+	}
+	public void setExcretionDetail(String excretion_detail){
+		this.excretion_detail=excretion_detail;
+	}
 }
--- src/bean/Student.java
+++ src/bean/Student.java
@@ -1,5 +1,55 @@
 package bean;
 
-public class Student {
+public class Student implements java.io.Serializable{
 
+	private String student_id;
+	private String student_name;
+	private String password;
+	private String class_id;
+	private String guardian_id;
+	private int flag;
+	private String student_record_id;
+
+	public String getStudentId(){
+		return student_id;
+	}
+	public String getStudentName(){
+		return student_name;
+	}
+	public String getPassword(){
+		return password;
+	}
+	public String getClassId(){
+		return class_id;
+	}
+	public String getGuardianId(){
+		return guardian_id;
+	}
+	public int getFlag(){
+		return flag;
+	}
+	public String getStudentRecordId(){
+		return student_record_id;
+	}
+	public void setStudentId(String student_id){
+		this.student_id=student_id;
+	}
+	public void setStudentName(String student_name){
+		this.student_name=student_name;
+	}
+	public void setPassword(String password){
+		this.password=password;
+	}
+	public void setClassId(String class_id){
+		this.class_id=class_id;
+	}
+	public void setGuardianId(String guardian_id){
+		this.guardian_id=guardian_id;
+	}
+	public void setFlag(int flag){
+		this.flag=flag;
+	}
+	public void setStudentRecordId(String student_record_id){
+		this.student_record_id=student_record_id;
+	}
 }
