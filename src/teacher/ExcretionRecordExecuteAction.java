aac3adf4bf033be02c30531cc025aadb77455ea8
tanihara <aa@a.com>
2024-11-14 14:05:12 +0900

    seikatukiroku

--- src/teacher/ExcretionRecordExecuteAction.java
+++ src/teacher/ExcretionRecordExecuteAction.java
@@ -7,38 +7,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.MealRecord;
-import dao.MealRecordDao;
+import bean.ExcretionRecord;
+import dao.ExcretionRecordDao;
 import tool.Action;
 
-
-public class MealRecordExecuteAction extends Action {
+public class ExcretionRecordExecuteAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		//ローカル変数の宣言 1
 		String url = "";
-		String meal_id = ""; //食事記録
+		String excretion_id = ""; //排泄記録
 		String student_id = ""; //生徒ID
-		MealRecordDao mealrecordDao = new MealRecordDao();
-		MealRecord mealrecord = null;
+		ExcretionRecordDao excretionrecordDao = new ExcretionRecordDao();
+		ExcretionRecord excretionrecord = null;
 
 		//リクエストパラメータ―の取得 2
-		meal_id = req.getParameter("meal_id");// 食事記録ID
+		excretion_id = req.getParameter("excretion_id");// 排泄記録ID
 		student_id = req.getParameter("student_id");//生徒ID
 
 		//DBからデータ取得 3
-		mealrecord = MealRecordDao.login(meal_id, student_id);//データ取得
+		excretionrecord = ExcretionRecordDao.login(excretion_id, student_id);//データ取得
 
 
 		//条件で手順4~7の内容が分岐
-		if (mealrecord != null) {// 認証成功の場合
+		if (excretionrecord != null) {// 認証成功の場合
 			// セッション情報を取得
 			HttpSession session = req.getSession(true);
 			// 認証済みフラグを立てる
 			//mealrecord.setAuthenticated(true);
 			// セッションにログイン情報を保存
-			session.setAttribute("user", mealrecord);
+			session.setAttribute("user", excretionrecord);
 
 			//リダイレクト
 			url = "";
@@ -50,7 +49,7 @@ public class MealRecordExecuteAction extends Action {
 			errors.add("IDまたはパスワードが確認できませんでした");
 			req.setAttribute("errors", errors);
 			// 入力されたIDをセット
-			req.setAttribute("meal_id", meal_id);
+			req.setAttribute("excretion_id", excretion_id);
 			//フォワード
 			url = "seikatukiroku-error.jsp";
 			req.getRequestDispatcher(url).forward(req, res);
@@ -59,3 +58,4 @@ public class MealRecordExecuteAction extends Action {
 	}
 
 }
+
--- src/teacher/SleepRecordExecuteAction.java
+++ src/teacher/SleepRecordExecuteAction.java
@@ -7,38 +7,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.MealRecord;
-import dao.MealRecordDao;
+import bean.SleepRecord;
+import dao.SleepRecordDao;
 import tool.Action;
 
 
-public class MealRecordExecuteAction extends Action {
+public class SleepRecordExecuteAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		//ローカル変数の宣言 1
 		String url = "";
-		String meal_id = ""; //食事記録
+		String sleep_id = ""; //睡眠記録
 		String student_id = ""; //生徒ID
-		MealRecordDao mealrecordDao = new MealRecordDao();
-		MealRecord mealrecord = null;
+		SleepRecordDao sleeprecordDao = new SleepRecordDao();
+		SleepRecord sleeprecord = null;
 
 		//リクエストパラメータ―の取得 2
-		meal_id = req.getParameter("meal_id");// 食事記録ID
+		sleep_id = req.getParameter("sleep_id");// 睡眠記録ID
 		student_id = req.getParameter("student_id");//生徒ID
 
 		//DBからデータ取得 3
-		mealrecord = MealRecordDao.login(meal_id, student_id);//データ取得
+		sleeprecord = SleepRecordDao.login(sleep_id, student_id);//データ取得
 
 
 		//条件で手順4~7の内容が分岐
-		if (mealrecord != null) {// 認証成功の場合
+		if (sleeprecord != null) {// 認証成功の場合
 			// セッション情報を取得
 			HttpSession session = req.getSession(true);
 			// 認証済みフラグを立てる
 			//mealrecord.setAuthenticated(true);
 			// セッションにログイン情報を保存
-			session.setAttribute("user", mealrecord);
+			session.setAttribute("user", sleeprecord);
 
 			//リダイレクト
 			url = "";
@@ -50,7 +50,7 @@ public class MealRecordExecuteAction extends Action {
 			errors.add("IDまたはパスワードが確認できませんでした");
 			req.setAttribute("errors", errors);
 			// 入力されたIDをセット
-			req.setAttribute("meal_id", meal_id);
+			req.setAttribute("sleep_id", sleep_id);
 			//フォワード
 			url = "seikatukiroku-error.jsp";
 			req.getRequestDispatcher(url).forward(req, res);
