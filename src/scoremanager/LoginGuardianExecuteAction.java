5d059d31b7deb8b2d909a8407b61f65c66f3ce32
nao-0802 <tky2302123@stu.o-hara.ac.jp>
2024-11-18 14:05:45 +0900

    返信!!

--- src/scoremanager/LoginGuardianExecuteAction.java
+++ src/scoremanager/LoginGuardianExecuteAction.java
@@ -7,16 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Admin;
 import bean.Guardian;
-import bean.Teacher;
-import dao.AdminDao;
 import dao.GuardianDao;
-import dao.TeacherDao;
 import tool.Action;
 
 
-public class LoginExecuteAction_admin extends Action {
+public class LoginGuardianExecuteAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
@@ -24,37 +20,32 @@ public class LoginExecuteAction_admin extends Action {
 		String url = "";
 		String id = "";
 		String password = "";
-		AdminDao adminDao = new AdminDao();
-		Admin admin = null;
-		TeacherDao teacherDao = new TeacherDao();
 		GuardianDao guardianDao = new GuardianDao();
+		Guardian guardian = null;
 
 		//リクエストパラメータ―の取得 2
-		id = req.getParameter("admin_id");// 管理者ID
+		id = req.getParameter("guardianID");// 管理者ID
 		password = req.getParameter("password");//パスワード
 
 		//DBからデータ取得 3
-		admin = adminDao.login(id, password);//管理者データ取得
-		List<Teacher>teacher = teacherDao.getAllTeachers();//教職員データ取得
-		List<Guardian>guardian = guardianDao.getAllGuardians();//保護者データ取得
+		guardian = guardianDao.login(id, password);//教員データ取得
 
 		//ビジネスロジック 4
 		//DBへデータ保存 5
 		//レスポンス値をセット 6
 		//フォワード 7
 		//条件で手順4~7の内容が分岐
-		if (admin != null) {// 認証成功の場合
+		if (guardian != null) {// 認証成功の場合
 			// セッション情報を取得
 			HttpSession session = req.getSession(true);
 			// 認証済みフラグを立てる
-			//admin.setAuthenticated(true);
+			//guardian.setAuthenticated(true);
 			// セッションにログイン情報を保存
-			session.setAttribute("user", admin);
-			req.setAttribute("teacher", teacher);
-			req.setAttribute("guardian", guardian);
+			session.setAttribute("user", guardian);
 
-			req.getRequestDispatcher("login-out.jsp")
-				.forward(req, res);
+			//リダイレクト
+			url = "main/Menu.action";
+			res.sendRedirect(url);
 		} else {
 			// 認証失敗の場合
 			// エラーメッセージをセット
@@ -62,7 +53,7 @@ public class LoginExecuteAction_admin extends Action {
 			errors.add("IDまたはパスワードが確認できませんでした");
 			req.setAttribute("errors", errors);
 			// 入力された教員IDをセット
-			req.setAttribute("adminID", id);
+			req.setAttribute("guardianID", id);
 			//フォワード
 			url = "login-error.jsp";
 			req.getRequestDispatcher(url).forward(req, res);
--- src/scoremanager/LoginTeacherAction.java
+++ src/scoremanager/LoginTeacherAction.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import tool.Action;
 
-public class LoginAction_guardian extends Action {
+public class LoginTeacherAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res)
 			throws Exception {
