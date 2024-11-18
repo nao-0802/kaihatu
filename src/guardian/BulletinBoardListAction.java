8c8b2ebbcbd9500512dbccd5eadfd4ba9f65820c
tanihara <aa@a.com>
2024-11-18 11:21:20 +0900

    hogosya

--- src/guardian/BulletinBoardListAction.java
+++ src/guardian/BulletinBoardListAction.java
@@ -1,5 +1,21 @@
 package guardian;
 
-public class BulletinBoardListAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BulletinBoardDao;
+import tool.Action;
+
+public class BulletinBoardListAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res)
+			throws Exception {
+		String post_id = req.getParameter("post_id");
+		BulletinBoardDao dao = new BulletinBoardDao();
+
+		//JSPへフォワード 7
+		req.getRequestDispatcher("bulletinboard.jsp").forward(req, res);
+	}
 
 }
