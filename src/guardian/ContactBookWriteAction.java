d11e9cb7abf0b8c3e828b680d0513452c4bf1e80
tanihara <aa@a.com>
2024-11-18 15:04:34 +0900

    ?

--- src/guardian/ContactBookWriteAction.java
+++ src/guardian/ContactBookWriteAction.java
@@ -1,5 +1,26 @@
 package guardian;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.ContactBook;
+import dao.ContactBookDao;
+
 public class ContactBookWriteAction {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res)
+			throws Exception {
+
+		String guardian_id = req.getParameter("guardian_id");
+		ContactBookDao dao = new ContactBookDao();
+		List<ContactBook> list=dao.serch("");
+		req.setAttribute("list", list);
+
+		//JSPへフォワード 7
+		req.getRequestDispatcher("contactbook.jsp").forward(req, res);
+	}
 
 }
